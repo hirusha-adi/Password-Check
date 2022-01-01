fdd893705e6204f6caf1fc9668fadb5e32cbaf9c
ZeaCeR#5641 <36286877+hirusha-adi@users.noreply.github.com>
2022-01-01 14:50:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ TEN_MIL = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Pass
 print("* Starting to download: 10-million-password-list-top-1000000.txt")
 r1 = requests.get(TEN_MIL).text
 print("* Writing downloaded data to file")
-with open(f"{passwords_dir}{SLASH}10-million-password-list-top-1000000.txt", "r", encoding="utf-8") as tenMilFile:
+with open(f"{passwords_dir}{SLASH}10-million-password-list-top-1000000.txt", "w", encoding="utf-8") as tenMilFile:
     tenMilFile.write(r1)
 print("* Completed: 10-million-password-list-top-1000000.txt")
 
@@ -27,7 +27,7 @@ if q1.lower().strip().startswith("y"):
     print("* Starting to download: darkweb2017-top10000.txt")
     r1 = requests.get(TEN_K).text
     print("* Writing downloaded data to file")
-    with open(f"{passwords_dir}{SLASH}darkweb2017-top10000.txt", "r", encoding="utf-8") as tenMilFile:
+    with open(f"{passwords_dir}{SLASH}darkweb2017-top10000.txt", "w", encoding="utf-8") as tenMilFile:
         tenMilFile.write(r1)
     print("* Completed: darkweb2017-top10000.txt")
 
